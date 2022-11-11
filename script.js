9cdcc1fcef394334a53c6fc318e847ad0fecbd7b
OlegFC <grinenkofreshcode@gmail.com>
2022-11-11 14:32:42 +0200

    newCode

--- script.js
+++ script.js
@@ -11,20 +11,21 @@ if (
 }
 function showOddEven(value) {
   if (userInput % 2 === 0) {
-    return "odd";
+    return "even";
   }
-  return "even";
+  return "odd";
 }
-console.log(showOddEven()); 
+console.log(showOddEven(userInput)); 
 
 // Task 2. написати функцію, яка приймає два аргументи і, якщо аргументи одного типу, то повертає той, який має більше значення (якщо значення рівні - повертаємо null), інакше(якщо різного типу) теж повертає null.
 
 const getMaxCompare = function (val1 = 12, val2 = "hello") {
-    if ((typeof val1 === typeof val2) && ((val1 > val2 || val2 > val1)) ) {
-      return 
-    }
-    if ((val1 === typeof val2) || (val1 ===  val2)) {
-      return null
-    } 
-    };
-    console.log(getMaxCompare());
+  if ((typeof val1 === typeof val2) && ((val1 > val2 || val2 > val1)) ) {
+     if(val1 > val2){return val1}
+     if(val2 > val1){return val2}
+  }
+
+    return null
+ 
+  };
+  console.log(getMaxCompare());
